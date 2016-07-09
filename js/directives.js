5fded8d287eea05a3f835445ad0415d359560309
Gosteeva <olia.gosteeva@gmail.com>
2016-07-09 21:00:45 +0300

    little style improvements

--- js/directives.js
+++ js/directives.js
@@ -7,11 +7,18 @@
       templateUrl: '/templates/list-temp.html'
     };
   });
-  
+
   app.directive('sideNav', function(){
     return{
       restrict: 'E',
       templateUrl: '/templates/sidenav-temp.html'
+    };
+  });
+
+  app.directive('listComment', function(){
+    return{
+      restrict: 'E',
+      templateUrl: '/templates/comments-list-temp.html'
     };
   });
 })();
